adb69574306251b4e8315d5d574ca74204a111eb
nickjgski <33236986+nickjgski@users.noreply.github.com>
2019-03-29 17:56:11 -0400

    Initial commit of HokieComposer

--- DnDCompanion/app/src/main/java/com/nickjgski/dndcompanion/DetailFragment.kt
+++ DnDCompanion/app/src/main/java/com/nickjgski/dndcompanion/DetailFragment.kt
@@ -3,13 +3,30 @@ package com.nickjgski.dndcompanion
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProviders
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 
 class DetailFragment : Fragment() {
+
+    var name: String? = null
+    var charClass: String? = null
+    var charRace: String? = null
+    var level: Int? = null
+    var str: Int? = null
+    var dex: Int? = null
+    var con: Int? = null
+    var int: Int? = null
+    var wis: Int? = null
+    var cha: Int? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +34,22 @@ class DetailFragment : Fragment() {
     ): View? {
         var view = inflater.inflate(R.layout.fragment_detail, container, false)
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+        name = this?.arguments?.getString("name")
+        charClass = this?.arguments?.getString("charClass")
+        charRace = null
+        level = null
+        str = null
+        dex = null
+        con = null
+        int = null
+        wis = null
+        cha = null
+        
     }
 
 }
--- DnDCompanion/app/src/main/java/com/nickjgski/dndcompanion/ListFragment.kt
+++ DnDCompanion/app/src/main/java/com/nickjgski/dndcompanion/ListFragment.kt
@@ -9,6 +9,7 @@ import androidx.lifecycle.ViewModelProviders.*
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.os.bundleOf
 import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -62,6 +63,15 @@ class ListFragment : Fragment() {
             holder.view.findViewById<TextView>(R.id.title).text = chars[position].name
             holder.view.findViewById<TextView>(R.id.race_class_info).text =
                 """${chars[position].race} ${chars[position].charClass}"""
+            holder.itemView.setOnClickListener{
+                holder.view.findNavController().navigate(R.id.action_listFragment_to_detailFragment,
+                    bundleOf("name" to chars[position].name, "charClass" to chars[position].charClass,
+                        "race" to chars[position].race, "level" to chars[position].level,
+                        "strength" to chars[position].str, "dexterity" to chars[position].dex,
+                        "constitution" to chars[position].con, "intelligence" to chars[position].int,
+                        "wisdom" to chars[position].wis, "charisma" to chars[position].cha)
+                )
+            }
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
